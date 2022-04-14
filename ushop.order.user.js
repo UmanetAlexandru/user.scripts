1f46898a6c6cd401923f2a0b84388793cb28b16f
Alexandru Umanet <alexandru.umanet@gmail.com>
2022-04-14 19:43:36 +0300

    showing the US shoe size on order page.

--- ushop.order.user.js
+++ ushop.order.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UShop Script
 // @namespace    https://github.com/UmanetAlexandru/user.scripts
-// @version      0.6
+// @version      0.7
 // @description  Script to copy UShop orders
 // @author       Alexandru Umaneț
 // @match        https://www.ushop.md/wp-admin/post.php?post=*
@@ -12,127 +12,134 @@
 // @run-at       document-end
 // @require      file://D:\projects\Personal\Scripts\ushop.order.user.js
 // ==/UserScript==
-(function () {
-    'use strict';
-    if (document.querySelector(".woocommerce-layout__header-wrapper h1").textContent !== "Edit Order") {
-        return;
-    }
-    const createCopyBtn = (name, copy) => {
-        const btn = document.createElement("a");
-        btn.innerText = name;
-        btn.classList.add("page-title-action");
-        btn.onclick = () => navigator.clipboard.writeText(copy);
-        const parentEl = document.querySelector(".wrap");
-        const beforeEl = document.querySelector("hr.wp-header-end");
-        parentEl.insertBefore(btn, beforeEl);
-    }
+const prefix = "https://raw.githubusercontent.com/UmanetAlexandru/user.scripts/master/resources/";
 
-    const date = document.querySelector("input[name=order_date]").getAttribute("value");
-    const firstName = document.querySelector("p._billing_first_name_field input").getAttribute("value");
-    const lastName = document.querySelector("p._billing_last_name_field input").getAttribute("value");
+const createCopyBtn = (name, copy) => {
+    const btn = document.createElement("a");
+    btn.innerText = name;
+    btn.classList.add("page-title-action");
+    btn.onclick = () => navigator.clipboard.writeText(copy);
+    const parentEl = document.querySelector(".wrap");
+    const beforeEl = document.querySelector("hr.wp-header-end");
+    parentEl.insertBefore(btn, beforeEl);
+}
+
+const getJSON = (url) => new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            resolve(xhr.response);
+        } else {
+            reject({
+                status: xhr.status,
+                statusText: xhr.statusText
+            });
+        }
+    };
+    xhr.send();
+});
+
+const skuFuncMap = {
+    "SPM": (sku) => {
+        const s = sku.split("_");
+        return `https://www.6pm.com/product/${s[1]}/color/${s[2]}`;
+    },
+    "ZP": (sku) => {
+        const s = sku.split("_");
+        return `https://www.zappos.com/product/${s[1]}/color/${s[2]}`;
+    },
+    "PMA": (sku) => `https://pumamoldova.md/ro/shop/${sku.replace("PMA_", "").replaceAll("_", "/")}`,
+    "SPD": (sku) => "https://www.sportsdirect.com/searchresults?descriptionfilter=" + sku.replace("SPD_", ""),
+    "JNB": (sku) => {
+        const s = sku.split("_");
+        return `https://www.joesnewbalanceoutlet.com/product/${s[1]}/${s[2]}`;
+    }
+}
+
+const addLinkToProd = (el) => {
+    const sku = el.childNodes[1].textContent.trim();
+    const link = skuFuncMap[sku.split("_")[0]](sku);
+    el.onclick = () => window.open(link, "_blank");
+    el.classList.add("page-title-action");
+    el.style.cssText += 'display:block;width:fit-content;padding-bottom:0;padding-top:0;margin-left:0;';
+}
+
+const eurSizes = await getJSON(prefix + "eur_sizes.json");
+
+const addUsSize = (prodEl, brand, gender) => {
+    const sizeEl = prodEl.querySelector("div.view p");
+    const eurSize = sizeEl.textContent.trim();
+    let key = `${gender}_${brand}_${eurSize}`.toUpperCase();
+    let usSize = eurSizes[key];
+    if (!usSize) {
+        key = `${gender}_NIKE_${eurSize}`.toUpperCase();
+        usSize = eurSize[key];
+    }
+    sizeEl.textContent = `${eurSize}\t(${usSize})`;
+}
+
+const shoesCategories = ["Bocanci/Cizme", "Ghete", "Pantofi/Loafers", "Sandale/Șlapi"]
+
+
+const app = async () => {
+    const firstName = document.getElementById("_billing_first_name").getAttribute("value");
+    const lastName = document.getElementById("_billing_last_name").getAttribute("value");
     const fullName = `${firstName} ${lastName}`;
+    const date = document.querySelector("input[name=order_date]").getAttribute("value");
 
     //<--------------- CREATE COPY ORDER INFO BUTTON
     const supplierMap = {"SPM": "6PM", "ZP": "Zappos", "PMA": "Puma", "SPD": "SD", "JNB": "JNB"};
     const number = document.querySelector("h2.woocommerce-order-data__heading").textContent
-        .replace("Order ", "")
-        .replace(" details", "")
-        .trim();
+        .replace("Order ", "").replace(" details", "").trim();
     const itemEls = document.querySelectorAll("table.woocommerce_order_items tr.item");
 
     const shippingTxt = document.querySelector(".shipping .edit input.wc_input_price").getAttribute("value");
     const shipping = +shippingTxt.replace(',', '.');
     const shippingPerItem = shipping / itemEls.length;
 
+    const brand = await getJSON(prefix + "brands.json");
+
     let orderInfo = "";
     itemEls.forEach(itemEl => {
-        const productName = itemEl.querySelector(".wc-order-item-name").textContent.trim();
-        const supplierPrefix = itemEl.querySelector(".wc-order-item-sku").childNodes[1].textContent
-            .trim()
-            .split("_")[0]
-        const supplier = supplierMap[supplierPrefix];
+        let productName = itemEl.querySelector(".wc-order-item-name").textContent.trim();
+        const prodBrand = brand.find(b => productName.startsWith(b));
+        productName = productName.replace(prodBrand, "");
+        let skuEl = itemEl.querySelector(".wc-order-item-sku");
+        addLinkToProd(skuEl);//ADDING LINK TO PRODUCT
+        const supplierPrefix = skuEl.childNodes[1].textContent.trim().split("_")[0];
         const size = itemEl.querySelector(".view p").textContent.trim();
-        const price = +itemEl.querySelector(".item_cost bdi").childNodes[0].textContent
-            .replace(",00 ", "").replace(".", "") + shippingPerItem;
-
-        orderInfo += `${number}\t${date}\t\t\t${productName}\t${size}\t${price}\t${supplier}\t${fullName}\n`;
-    });
-    createCopyBtn("Copy Order Info", orderInfo)
-
-    //<--------- ADD LINKS TO PRODUCT ITEMS
-    const skuFuncMap = {
-        "SPM": (sku) => {
-            const s = sku.split("_");
-            return `https://www.6pm.com/product/${s[1]}/color/${s[2]}`;
-        },
-        "ZP": (sku) => {
-            const s = sku.split("_");
-            return `https://www.zappos.com/product/${s[1]}/color/${s[2]}`;
-        },
-        "PMA": (sku) => `https://pumamoldova.md/ro/shop/${sku.replace("PMA_", "").replaceAll("_", "/")}`,
-        "SPD": (sku) => "https://www.sportsdirect.com/searchresults?descriptionfilter=" + sku.replace("SPD_", ""),
-        "JNB": (sku) => {
-            const s = sku.split("_");
-            return `https://www.joesnewbalanceoutlet.com/product/${s[1]}/${s[2]}`;
+        if (prodBrand !== "Puma" && shoesCategories.find(c => productName.includes(c))) {//ADDING US SIZE
+            const gender = productName.includes("Băieţi") ? "MEN" : "WOMEN";
+            addUsSize(itemEl, prodBrand, gender);
         }
-    }
-
-    document.querySelectorAll(".wc-order-item-sku").forEach(el => {
-        const sku = el.childNodes[1].textContent.trim();
-        const link = skuFuncMap[sku.split("_")[0]](sku);
-        el.onclick = () => window.open(link, "_blank");
-        el.classList.add("page-title-action");
-        el.style.cssText += 'display:block;width:fit-content;padding-bottom:0;padding-top:0;margin-left:0;';
+        const supplier = supplierMap[supplierPrefix];
+        const price = itemEl.querySelector(".item_cost bdi").childNodes[0].textContent
+            .replace(",00 ", "").replace(".", "") + shippingPerItem;
+        console.log(size);
+        orderInfo += `${number}\t${date}\t\t${prodBrand}\t${productName}\t${size}\t${price}\t${supplier}\t${fullName}\n`;
     });
+    createCopyBtn("Copy Order Info", orderInfo);
 
     //<--------------- CREATE COPY CUSTOMER INFO BUTTON
-    const statesMap = {
-        "C": "Chișinău",
-        "BL": "Bălți",
-        "AN": "Anenii Noi",
-        "BS": "Basarabeasca",
-        "BR": "Briceni",
-        "CH": "Cahul",
-        "CT": "Cantemir",
-        "CL": "Călărași",
-        "CS": "Căușeni",
-        "CM": "Cimișlia",
-        "CR": "Criuleni",
-        "DN": "Dondușeni",
-        "DR": "Drochia",
-        "DB": "Dubăsari",
-        "ED": "Edineț",
-        "FL": "Fălești",
-        "FR": "Florești",
-        "GE": "UTA Găgăuzia",
-        "GL": "Glodeni",
-        "HN": "Hîncești",
-        "IL": "Ialoveni",
-        "LV": "Leova",
-        "NS": "Nisporeni",
-        "OC": "Ocnița",
-        "OR": "Orhei",
-        "RZ": "Rezina",
-        "RS": "Rîșcani",
-        "SG": "Sîngerei",
-        "SR": "Soroca",
-        "ST": "Strășeni",
-        "SD": "Șoldănești",
-        "SV": "Ștefan Vodă",
-        "TR": "Taraclia",
-        "TL": "Telenești",
-        "UN": "Ungheni"
-    }
     const lang = document.querySelector(".order_notes li:last-child p").textContent.trim().includes("Оплата") ? "Ru" : "Ro";
-    const email = document.querySelector("._billing_email_field input").getAttribute("value");
-    const cityCode = document.querySelector("._billing_state_field input").getAttribute("value");
-    const address = document.querySelector("._billing_address_1_field input").getAttribute("value");
-    let phone = document.querySelector("._billing_phone_field input").getAttribute("value");
+    const email = document.getElementById("_billing_email").getAttribute("value");
+    const city = document.getElementById("select2-_billing_state-container").textContent;
+    const address = document.getElementById("_billing_address_1").getAttribute("value");
+    let phone = document.getElementById("_billing_phone").getAttribute("value");
     phone = '0' + phone.replace(/^\s*\+?(373)? ?0?/, "");
     phone = phone.slice(0, 4) + ' ' + phone.slice(4, 6) + ' ' + phone.slice(6);
-    const city = statesMap[cityCode];
     const gender = firstName.endsWith('a') ? "W" : "M";
 
     const customerInfo = `${email}\t${firstName}\t${lastName}\t${date}\t${lang}\t${gender}\t${city}\t\t${address}\tSubscribed\t${phone}`;
     createCopyBtn("Copy Customer Info", customerInfo);
+}
+
+(() => {
+    'use strict';
+    if (document.querySelector(".woocommerce-layout__header-wrapper h1").textContent !== "Edit Order") {
+        return;
+    }
+    window.addEventListener('load', app, false);
 })();
